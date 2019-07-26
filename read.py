7f9a4f11d0850612018b78666eacb6f39c0f7dbf
Chris Wu <christian.wucy@gmail.com>
2019-07-26 19:13:51 +0800

    update data len avg

--- read.py
+++ read.py
@@ -7,7 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: # % 是求餘數
 			print(len(data))
 			# 把進度印出來，但print很花時間，所以每1000筆印一次
-print(len(data))
-print(data[0])
-print('-------------------')
-print(data[1])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for dat in data:
+	sum_len += len(dat)
+	#print(sum_len)
+print('分析完畢！平均每筆資料長度為：', sum_len/len(data))
