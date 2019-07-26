cf4159f90575afb8c68d0d548ec869e358c14c14
Chris Wu <christian.wucy@gmail.com>
2019-07-26 21:30:53 +0800

    add filter function

--- read.py
+++ read.py
@@ -14,3 +14,11 @@ for dat in data:
 	sum_len += len(dat)
 	#print(sum_len)
 print('分析完畢！平均每筆資料長度為：', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+#print(new[0])
+#print(new[1])
