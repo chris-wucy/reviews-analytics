9bc33a5c5a08203bc79dc348920b01e96a9c10b9
Chris Wu <christian.wucy@gmail.com>
2019-07-29 16:24:59 +0800

    add word count

--- read.py
+++ read.py
@@ -8,7 +8,10 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 			# 把進度印出來，但print很花時間，所以每1000筆印一次
 print('檔案讀取完了, 總共有', len(data), '筆資料')
+print(data[0])
 
+# command + / 
+# -------------------- 
 sum_len = 0
 for dat in data:
 	sum_len += len(dat)
@@ -47,6 +50,32 @@ bad = ['bad' in d for d in data]
 # 每一個在data裡的都做 'bad' in d 的判斷，True or False
 print(bad)
 
+# 算各個字出現的次數，用字典
+
+wc = {} # word_count
+for d in data:
+	words = d.strip().split() # split拿掉' ', 用預設值，就不會出現空字串
+	for word in words:
+		if word in wc: # 沒有的話可以用not in
+			wc[word] += 1
+		else:
+			wc[word] = 1 # 沒有就設成1，第1次出現
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])	
+print(len(wc))
+
+while True:
+	word = input('請問您想查什麼字： ')
+	if word == 'q':
+		break
+	if word not in wc:
+		print('查無此字')
+	print(word, '出現過的次數為：', wc[word])
+print('感謝使用本查詢功能')
+
+
+# 使用者輸入想查某個字出現的次數
 
 #print(good[0])
 #print(new[1])
